c3938b50288763f509b79d61b82d83e6802cabfd
hujinbin <437044048@qq.com>
2020-06-14 19:17:12 +0800

    edit

--- docs/.vuepress/components/heaven/icon.vue
+++ docs/.vuepress/components/heaven/icon.vue
@@ -1,9 +1,37 @@
 <template>
   <div>
-    <!-- <h-icon name="user"><h-icon>
-    <h-icon name="close"><h-icon>
-    <h-icon name="loading"><h-icon>
-    <h-icon name="set"><h-icon> -->
+    <div>
+     <h-icon name="user"><h-icon>
+      <p>user</p>
+    </div>
+    <div>
+     <h-icon name="close"><h-icon>
+      <p>close</p>
+    </div>
+    <div>
+     <h-icon name="loading"><h-icon>
+      <p>loading</p>
+    </div>
+    <div>
+     <h-icon name="set"><h-icon>
+      <p>set</p>
+    </div>
+     <div>
+     <h-icon name="star-on"><h-icon>
+      <p>star-on</p>
+    </div>
+     <div>
+     <h-icon name="star-off"><h-icon>
+      <p>star-off</p>
+    </div>
+      <div>
+     <h-icon name="more"><h-icon>
+      <p>more</p>
+    </div>
+      <div>
+     <h-icon name="del"><h-icon>
+      <p>del</p>
+    </div>
   </div>
 </template>
 
